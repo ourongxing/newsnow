bc5464cd7b95b4f404adfd8ee6d2f43232d44dd8
Wakler-Wei W <319230300@qq.com>
2025-04-03 07:56:58 +0000

    fix-name

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -46,16 +46,16 @@ export function Header() {
         <Link to="/" className="flex gap-2 items-center">
           <div className="h-10 w-10 bg-cover" title="logo" style={{ backgroundImage: "url(/icon.svg)" }} />
           <span className="text-2xl font-brand line-height-none!">
-            <p>News</p>
+            <p>Walker</p>
             <p className="mt--1">
               <span className="color-primary-6">N</span>
-              <span>ow</span>
+              <span>ews</span>
             </p>
           </span>
         </Link>
-        <a target="_blank" href={`${Homepage}/releases/tag/v${Version}`} className="btn text-sm ml-1 font-mono">
+        {/* <a target="_blank" href={`${Homepage}/releases/tag/v${Version}`} className="btn text-sm ml-1 font-mono">
           {`v${Version}`}
-        </a>
+        </a> */}
       </span>
       <span className="justify-self-center">
         <span className="hidden md:(inline-block)">

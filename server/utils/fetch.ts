64c4090f93c8bf149440ec69bcc6b927ff1af535
zhengningning <zhengningning@youzan.com>
2025-07-31 14:09:42 +0800

    feat: Add proxy support to `$fetch` and update dependencies
    - Introduced proxy handling for `$fetch` with options for HTTP and HTTPS proxies using `undici`'s `ProxyAgent`.
    - Configured `.env` variables to handle proxy settings (`HTTP_PROXY`, `HTTPS_PROXY`, `PROXY_LINKS`).
    - Updated `docker-compose.yml` to include new environment variables.
    - Added `undici` to dependencies, updated associated lock files.
    - Enhanced `.gitignore` for development-related files.

--- server/utils/fetch.ts
+++ server/utils/fetch.ts
@@ -1,9 +1,50 @@
-import { $fetch } from "ofetch"
+import {$Fetch, $fetch} from "ofetch"
+import { ProxyAgent } from "undici";
+import process from "node:process";
 
-export const myFetch = $fetch.create({
+const COMMON_PARAMS = {
   headers: {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
   },
   timeout: 10000,
   retry: 3,
-})
+};
+
+const HTTP_PROXY = process.env.HTTP_PROXY;
+const HTTPS_PROXY = process.env.HTTPS_PROXY;
+const PROXY_LINKS = (process.env.PROXY_LINKS || '').split(',');
+
+const proxyHttpAgent = HTTP_PROXY && new ProxyAgent(HTTP_PROXY);
+const proxyHttpsAgent = HTTPS_PROXY && new ProxyAgent(HTTPS_PROXY);
+
+export const baseMyyFetch = $fetch.create(COMMON_PARAMS)
+
+// @ts-ignore
+export const myFetch: $Fetch = (url, options, ...restParams) => {
+  const needProxy = PROXY_LINKS.some(item => {
+    if (typeof url === 'string') {
+      return url.includes(item);
+    }
+
+    return false;
+  });
+
+  if (!needProxy) {
+    return baseMyyFetch(url, options, ...restParams);
+  }
+
+  const isHttps = typeof url === 'string' && url.startsWith('https');
+  if (isHttps) {
+    return baseMyyFetch(url, {
+      ...options,
+      // @ts-ignore
+      dispatcher: proxyHttpsAgent ? proxyHttpsAgent : undefined
+    }, ...restParams)
+  }
+
+  return baseMyyFetch(url, {
+    ...options,
+    // @ts-ignore
+    dispatcher: proxyHttpAgent ? proxyHttpAgent : undefined
+  }, ...restParams);
+}

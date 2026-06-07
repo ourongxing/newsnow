289c2586f2012b70de3d5daec9bb15eb627691d3
Weijie Wang <weijie.wang@exfo.com>
2026-06-07 17:04:15 -0400

    eslint versioning

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,9 +3,17 @@ export function Footer() {
     <>
       <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">MIT LICENSE</a>
       <span>
-        <span>NewsNow © 2024 By </span>
+        <span>NewsNow © 2026 by </span>
         <a href={Author.url} target="_blank">
           {Author.name}
+        </a>
+        <span> · based on </span>
+        <a href={OriginalProject.url} target="_blank">
+          {OriginalProject.name}
+        </a>
+        <span> by </span>
+        <a href={OriginalAuthor.url} target="_blank">
+          {OriginalAuthor.name}
         </a>
       </span>
     </>
